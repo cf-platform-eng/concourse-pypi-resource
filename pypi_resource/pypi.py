aa71f01fd706f08ee0243f6a807c88da0f6f713f
Michael William Boldt <mike.boldt@gmail.com>
2017-09-20 21:31:33 -0500

    Restore get_pypi_url; still needed for check.

--- pypi_resource/pypi.py
+++ pypi_resource/pypi.py
@@ -16,6 +16,11 @@ from distutils.version import LooseVersion
 
 import requests
 
+def get_pypi_url(input):
+    if input['source']['test']:
+        return 'https://testpypi.python.org/pypi'
+    return 'https://pypi.python.org/pypi'
+
 def get_pypi_repository(input):
     if input['source']['test']:
         return 'pypitest'
