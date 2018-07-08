229cd4c281219240cf9a205ef2dabf38eb33efc0
punkadiddle <>
2018-07-08 23:42:51 +0200

    input parameters moved to repository dictionary

--- pypi_resource/out.py
+++ pypi_resource/out.py
@@ -19,39 +19,28 @@ import json
 import os
 import subprocess
 import sys
-import warnings
-from distutils.version import LooseVersion
-
-import pkginfo
 
 from . import common, pipio
 
 
-def get_package_version(pkgpath):
-    metadata = pkginfo.get_metadata(pkgpath)
-    return LooseVersion(metadata.version)
-
-
 def find_package(pattern, srcdir):
     files = glob.glob(os.path.join(srcdir, pattern))
-    warnings.warn('Glob {} matched files: {}'.format(pattern, files))
-    files = sorted(files, key=get_package_version)
+    common.msg('Glob {} matched files: {}', pattern, files)
+    files = sorted(files, key=common.get_package_version)
     return files[-1]
 
 
 def upload_package(pkgpath, input):
-    twine_cmd = [ 'twine', 'upload' ]
-    if 'repository_url' in input['source']:
-        twine_cmd.append('--repository-url')
-        twine_cmd.append(pipio.get_pypi_url(input, 'out'))
-    else:
-        twine_cmd.append('--repository')
-        twine_cmd.append(pipio.get_pypi_repository(input))
+    repocfg = input['source']['repository']
+    twine_cmd = ['twine', 'upload']
+
+    url, unused_hostname = pipio.get_pypi_url(input, 'out') 
+    twine_cmd.extend(['--repository-url', url])
 
     twine_cmd.append('--username')
-    twine_cmd.append(input['source'].get('username', os.getenv('TWINE_USERNAME')))
+    twine_cmd.append(repocfg.get('username', os.getenv('TWINE_USERNAME')))
     twine_cmd.append('--password')
-    twine_cmd.append(input['source'].get('password', os.getenv('TWINE_PASSWORD')))
+    twine_cmd.append(repocfg.get('password', os.getenv('TWINE_PASSWORD')))
     twine_cmd.append(pkgpath)
 
     subprocess.run(twine_cmd, stdout=sys.stderr.fileno(), check=True)
@@ -59,10 +48,10 @@ def upload_package(pkgpath, input):
 
 def out(srcdir, input):
     common.merge_defaults(input)
-    warnings.warn('Finding package to upload')
+    common.msg('Finding package to upload')
     pkgpath = find_package(input['params']['glob'], srcdir)
-    version = get_package_version(pkgpath)
-    warnings.warn('Uploading {} version {}'.format(pkgpath, version))
+    version = common.get_package_version(pkgpath)
+    common.msg('Uploading {} version {}', pkgpath, version)
     upload_package(pkgpath, input)
     return {'version': {'version': str(version)}}
 
